89f4f45f453673fa7777ec3540b07250fbdc16c0
Pau Moreno Andrés <pmoreno2022@ilg.cat>
2022-10-24 21:04:59 +0200

    cambio pau

--- java/Program.java
+++ java/Program.java
@@ -1,10 +1,10 @@
 /*
- * Program.java        20.10.2020
+ * Program12.java        20.10.2020
  *
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Ernesto diaz jimenez <jseculi@escoladeltreball.org>
+ * Copyright 2020 Pau <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
